4a71b22129ef9143f8c5bad95d03b3a6a235a20a
Sameer Ahmed <sameergubbok9@gmail.com>
2026-07-10 01:48:51 -0300

    Program in c add of 2 numbers

---
+++ add of 2 numbers.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void main(){
+//	This program will be run but the result will not be plus becouse the result is not plus
+//	int num1,num2,sum;
+//	
+//	printf("Enter first num for plus:");
+//	scanf("%d", &num1);
+//	printf("Enter second num for plus:");
+//	scanf("%d", &num2);
+//	printf("The result:%d", sum);
+	
+
+//This program is correct and it will show the result which will be plus because we have plus it
+
+	int num1,num2,sum;
+	
+	printf("Enter first num for plus:");
+	scanf("%d", &num1);
+	printf("Enter second num for plus:");
+	scanf("%d", &num2);
+	sum=num1+num2;
+	printf("The result:%d", sum);
+	 
+	
+	
+}
